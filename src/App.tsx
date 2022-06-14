56375acbd90cae88eb08d074efe35e4ea3b30e49
ranckprogram <1474574022@qq.com>
2022-06-14 21:10:17 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">dd</Button>
+    </div>
+  );
+}
+
+export default App;
