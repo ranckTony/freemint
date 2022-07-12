fd5fdede5947586fdeea86e467d9e28b21776c3e
ranckTony <zy17313019780@163.com>
2022-07-12 23:36:01 +0800

    add polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import "./polyfills";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
